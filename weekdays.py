1e4331a8574cc6bb34ca65fc492287eac4efa3cd
aveeralgaur <aveeral.firebolt@gmail.com>
2020-08-19 20:56:45 +0530

    Commit message

---
+++ weekdays.py
@@ -0,0 +1,17 @@
+user_input=int(input("Enter the day number: "))
+if user_input==1:
+    print("The day is Monday")
+elif user_input==2:
+    print("The day is Tuesday")
+elif user_input==3:
+    print("The day is Wednesday")
+elif user_input==4:
+    print("The day is Thursday")
+elif user_input==5:
+    print("The day is Friday")
+elif user_input==6:
+    print("The day is Saturday")
+elif user_input==7:
+    print("The day is Sunday")
+else:
+    print("No such day exists")
