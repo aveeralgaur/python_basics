40893c5eac67917874f5b7659855986c00abcc9e
Aveeral Gaur <55633374+aveeralgaur@users.noreply.github.com>
2021-06-07 18:20:48 +0530

    Update lists_greetings.py

--- lists_greetings.py
+++ lists_greetings.py
@@ -4,6 +4,6 @@ print(guests[3]+" "+"can't make it for dinner.Because I don't like him.")
 del guests[3]
 guests.insert(3,'JK')
 print(guests)
-print(guests[3]+" "+"takeovers instead of "+"Sundar"+".")
+print(guests[3]+" "+"takeovers instead of Sundar.")
 for i in guests:
     print("Hey"+" "+i+" I invite you to join me for the dinner today.")
